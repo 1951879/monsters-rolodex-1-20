47cb77dca3476e74f7928ac68f7b71cb5c420db4
Sergi Boyuk <sboyuk@outlook.com>
2020-01-31 16:24:35 -0500

    Updates

---
+++ precache-manifest.3ff6e8c7f067e5bd69eb51c23134077b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f39c766a963244fe663d355ad57fd158",
+    "url": "/monsters-rolodex-1-20/index.html"
+  },
+  {
+    "revision": "f7a1320a1f0295adcb76",
+    "url": "/monsters-rolodex-1-20/static/css/main.6cd04a71.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex-1-20/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex-1-20/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f7a1320a1f0295adcb76",
+    "url": "/monsters-rolodex-1-20/static/js/main.3c6748f4.chunk.js"
+  },
+  {
+    "revision": "88c3a9a1b57f55e0019d",
+    "url": "/monsters-rolodex-1-20/static/js/runtime-main.2e941ce3.js"
+  }
+]);
